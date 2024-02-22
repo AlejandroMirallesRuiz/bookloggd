de2354955144aeaa1c353a3ff421194c7e0f229e
AlejandroMirallesRuiz <mirall271828s@gmail.com>
2024-02-22 11:37:44 +0100

    Main routers created

--- Library/src/Controller/BookController.php
+++ Library/src/Controller/BookController.php
@@ -17,10 +17,41 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/book', name: 'app_book')]
-    public function book(): Response
+    #[Route('/book/{bookId}', name: 'app_book')]
+    public function book(int $bookId): Response
     {
         return $this->render('book/book.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route('/updateBook/{bookId}', methods: ['GET'],  name: 'app_updateBook')]
+    public function updateBook(int $bookId): Response
+    {
+        return $this->render('book/updateBook.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+    
+    #[Route('/updateBook/{bookId}', methods: ['POST'],  name: 'post_updateBook')]
+    public function post_UpdateBook(int $bookId): Response
+    {
+        return $this->render('book/updateBook.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route('/updateReadingStatus/{bookId}', methods: ['GET'], name: 'app_updateReadingStatus')]
+    public function updateReadingStatus(int $bookId): Response
+    {
+        return $this->render('book/updateReadingStatus.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+    #[Route('/updateReadingStatus/{bookId}', methods: ['POST'], name: 'post_updateReadingStatus')]
+    public function post_UpdateReadingStatus(int $bookId): Response
+    {
+        return $this->render('book/updateReadingStatus.html.twig', [
             'controller_name' => 'BookController',
         ]);
     }
